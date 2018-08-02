71bb39830a2c07ab17162eb9c30177c79f37f95a
Marcin Pabich <martinoqom@gmail.com>
2018-08-02 14:24:14 +0200

    Fixing message receiving and logging

--- src/main/java/it/unibo/springchat/controller/ChatController.java
+++ src/main/java/it/unibo/springchat/controller/ChatController.java
@@ -2,6 +2,8 @@ package it.unibo.springchat.controller;
 
 import static java.lang.String.format;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -16,26 +18,29 @@ import it.unibo.springchat.model.ChatMessage.MessageType;
 @Controller
 public class ChatController {
 
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+	
 	@Autowired
 	private SimpMessageSendingOperations messagingTemplate;
 
 	@MessageMapping("/chat/{roomId}/sendMessage")
 	public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
 		// TODO: Transforms to ordered chat messages
-		messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
+		messagingTemplate.convertAndSend(format("/topic/%s", roomId), chatMessage);
 	}
 
 	@MessageMapping("/chat/{roomId}/addUser")
-	public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
-			SimpMessageHeaderAccessor headerAccessor) {
-		final String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
-		if (currentRoomId != null) {
+	public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("room_id", roomId);
+		logger.info("User " + chatMessage.getSender() + " is trying to get the " + roomId);
+		if (roomId != null) {
 			final ChatMessage leaveMessage = new ChatMessage();
 			leaveMessage.setType(MessageType.JOIN);
 			leaveMessage.setSender(chatMessage.getSender());
-			messagingTemplate.convertAndSend(format("/channel/%s", currentRoomId), leaveMessage);
+			messagingTemplate.convertAndSend(format("/topic/%s", roomId), leaveMessage);
+			logger.info("User " + chatMessage.getSender() + " joined to the " + roomId + ". Sent a message to " + format("/topic/%s", roomId));
 		}
 		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-		messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
+		messagingTemplate.convertAndSend(format("/topic/%s", roomId), chatMessage);
 	}
 }
